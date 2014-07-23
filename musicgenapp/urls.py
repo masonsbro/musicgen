4498345f9493df155a346906827ba1c1b2dba817
Ethan Arnold <masonsbro456@gmail.com>
2014-07-22 23:26:19 -0500

    create good index page and starter about page

--- musicgenapp/urls.py
+++ musicgenapp/urls.py
@@ -4,5 +4,6 @@ from musicgenapp import views
 
 urlpatterns = patterns('',
 	#url(r'^(?P<course_id>[0-9]+)/$', views.courseDetails),
+	url(r'^about/$', views.about),
 	url(r'^$', views.index),
 )
--- musicgenapp/views.py
+++ musicgenapp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(req):
-    return render(req, "parent.html")
+	return render(req, "index.html")
+
+def about(req):
+	return render(req, "about.html")
