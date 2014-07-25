ca06aade9d52c0672f14a2f78a5ac1a8ca9dba62
Ethan Arnold <masonsbro456@gmail.com>
2014-07-25 14:45:56 -0500

    change static_root again

--- musicgen/settings.py
+++ musicgen/settings.py
@@ -98,7 +98,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
