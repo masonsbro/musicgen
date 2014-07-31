7fb7f33b0ef2737400f923e90a446f91a36e0be7
Ethan Arnold <masonsbro456@gmail.com>
2014-07-30 22:45:48 -0500

    debug = false

--- musicgen/settings.py
+++ musicgen/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'ri&o(@1puy7idrn!gtwrrx7o*d4=5=yvo)#m!icc()$7ng+)mr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
