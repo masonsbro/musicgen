4d1ade04b9a785df1e8989bdeeb0c34ee7dd0fc5
Ethan Arnold <masonsbro456@gmail.com>
2014-07-30 22:43:28 -0500

    fix js localhost

--- static/list.js
+++ static/list.js
@@ -1,6 +1,6 @@
 function rate(id) {
 	val = $("#rate" + id).val();
-	$.ajax("http://localhost:8000/rate/" + id + "/" + val + "/");
+	$.ajax("http://musicgen.herokuapp.com/rate/" + id + "/" + val + "/");
 	$("#rate" + id).prop("disabled", true);
 	$("#num" + id).html(parseInt($("#num" + id).html()) + 1);
 	return false;
