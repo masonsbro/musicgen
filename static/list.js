b0fab602fb61d30c0def95ae67293e002a58b74a
Ethan Arnold <masonsbro456@gmail.com>
2014-08-03 02:58:35 -0500

    Change list.js to point to musicgen.org for rating in session

--- static/list.js
+++ static/list.js
@@ -1,6 +1,6 @@
 function rate(id) {
 	val = $("#rate" + id).val();
-	$.ajax("http://musicgen.herokuapp.com/rate/" + id + "/" + val + "/");
+	$.ajax("http://www.musicgen.org/rate/" + id + "/" + val + "/");
 	$("#rate" + id).prop("disabled", true);
 	$("#num" + id).html(parseInt($("#num" + id).html()) + 1);
 	return false;
