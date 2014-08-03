5fb767956592190c21cbf95f06d3d94d518f5aaf
Ethan Arnold <masonsbro456@gmail.com>
2014-08-03 05:34:50 -0500

    ugh

---
+++ musicgen/settings.py
@@ -0,0 +1,128 @@
+"""
+Django settings for musicgen project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ri&o(@1puy7idrn!gtwrrx7o*d4=5=yvo)#m!icc()$7ng+)mr'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+    'musicgenapp',
+    'storages',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'musicgen.urls'
+
+WSGI_APPLICATION = 'musicgen.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'ec2-54-197-238-8.compute-1.amazonaws.com',
+        'PORT': '5432',
+        'NAME': 'd5pr2lc9n6k9h4',
+        'USER': 'kbkfmmtyebtojq',
+        'PASSWORD': 'OKOM9XAQrdJ_gngBI7tow_JoWB',
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'template'),
+)
+
+# django-storages
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# http instead of https
+AWS_S3_SECURE_URLS = False
+# don't add compelx auth-related query parameters for requests
+AWS_QUERYSTRING_AUTH = False
+# access key id
+AWS_S3_ACCESS_KEY_ID = 'AKIAJENMPVSPWEURXMHA'
+# secret access key
+AWS_S3_SECRET_ACCESS_KEY = '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'
+# storage bucket name
+AWS_STORAGE_BUCKET_NAME = 'musicgen'
+
+SG_USERNAME = "app27699523@heroku.com"
+SG_PASSWORD = "4tnoo5h6"
